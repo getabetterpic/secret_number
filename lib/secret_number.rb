e9d4dcb3a1673ab7ffe3a1e0dcf20b47fce88867
getabetterpic <dan@danandjensmith.com>
2016-06-14 12:03:36 -0400

    Add is_secret_additive method as well as secret method placeholder

--- lib/secret_number.rb
+++ lib/secret_number.rb
@@ -14,13 +14,34 @@ module SecretNumber
       raise IntegerNotFoundError, 'Sorry, the argument you pass must be an integer' unless @number =~ /\d+/
     end
 
-    def is_prime(test_number)
+    def is_prime(test_number = @number)
+      return false if test_number.abs < 2
       for number in 2..(test_number - 1)
         if (test_number % number) == 0
           return false
         end
       end
       return true
+    end
+
+    def is_secret_additive(test_number = @number)
+      prime_array = []
+      counter = 0
+      while counter < test_number
+        prime_array << counter if is_prime(counter)
+        counter += 1
+      end
+
+      prime_array.each do |x|
+        prime_array.each do |y|
+          return false if secret(x + y) != secret(x) + secret(y)
+        end
+      end
+      return true
+    end
+
+    def secret(integer)
+      integer
     end
   end
 end
