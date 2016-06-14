92ad9ae642aa95efc183fc7a9740c25d547e46ad
getabetterpic <dan@danandjensmith.com>
2016-06-14 10:39:09 -0400

    Start development on application

--- lib/secret_number.rb
+++ lib/secret_number.rb
@@ -1,5 +1,17 @@
-require "secret_number/version"
+require_relative "./secret_number/version"
 
 module SecretNumber
-  # Your code goes here...
+  class IntegerNotFoundError < StandardError; end
+  class NoArgumentFoundError < StandardError; end
+
+  class Application
+    def initialize(argv)
+      @number = argv[0]
+    end
+
+    def run
+      raise NoArgumentFoundError, 'You must supply an integer as an argument' if @number == nil
+      raise IntegerNotFoundError, 'Sorry, the argument you pass must be an integer' unless @number =~ /\d+/
+    end
+  end
 end
