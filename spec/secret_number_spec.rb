e37438245aa838d646568ed7c14615195976b2a1
getabetterpic <dan@danandjensmith.com>
2016-06-14 10:54:50 -0400

    Setup test for is_prime function

--- spec/secret_number_spec.rb
+++ spec/secret_number_spec.rb
@@ -13,4 +13,14 @@ describe SecretNumber do
     expect { SecretNumber::Application.new(['string']).run }.to raise_error(SecretNumber::IntegerNotFoundError)
     expect { SecretNumber::Application.new([1.23]).run }.to raise_error(SecretNumber::IntegerNotFoundError)
   end
+
+  context '#is_prime' do
+    it 'returns true if given number is prime' do
+      expect(SecretNumber::Application.new([5]).is_prime(5)).to eq(true)
+    end
+
+    it 'returns false if given number is not prime' do
+      expect(SecretNumber::Application.new([6]).is_prime(6)).to eq(false)
+    end
+  end
 end
