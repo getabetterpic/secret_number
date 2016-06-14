523a67ae9a6ca1011c26976736ace2eff9c2accd
getabetterpic <dan@danandjensmith.com>
2016-06-14 10:55:18 -0400

    Add is_prime function

--- lib/secret_number.rb
+++ lib/secret_number.rb
@@ -13,5 +13,14 @@ module SecretNumber
       raise NoArgumentFoundError, 'You must supply an integer as an argument' if @number == nil
       raise IntegerNotFoundError, 'Sorry, the argument you pass must be an integer' unless @number =~ /\d+/
     end
+
+    def is_prime(test_number)
+      for number in 2..(test_number - 1)
+        if (test_number % number) == 0
+          return false
+        end
+      end
+      return true
+    end
   end
 end
